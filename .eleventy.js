804decfddde746577dfa8df6ba21b1cfaccc5a39
Chris Hartgerink <chris@libscie.org>
2023-08-09 12:08:25 +0200

    Add passThroughCopy for all module info

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,5 @@ module.exports = function(eleventyConfig) {
     // Output directory: _site
     // Copy any .jpg file to `_site`, via Glob pattern
     // Keeps the same directory structure.
-    eleventyConfig.addPassthroughCopy("./modules/tkj9-d4kf/Anti_Oedipe_prelude_1_2_2022.docx");
+    eleventyConfig.addPassthroughCopy("./modules/**");
   };
